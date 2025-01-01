975b62e35291f64d4bfea8ca3f2e3c40fcdac087
Sachethanv <Sachethanv.codes@gmail.com>
2025-01-01 17:38:07 +0530

    Added Subject,links,topic variable and english news only

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import requests
 from mail import send_email
 
-
-
+topic = "cricket"
 api_key = "fcb0f287b9af4b698812a5172e49bb9d"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2024-12-01&sortBy=publishedAt&apiKey=fcb0f287b9af4b698812a5172e49bb9d"
+url = "https://newsapi.org/v2/everything?"\
+    f"q={topic}"\
+    "&from=2024-12-01"\
+    "&sortBy=publishedAt"\
+    "&apiKey=fcb0f287b9af4b698812a5172e49bb9d"\
+    "&language=en"
 
 request = requests.get(url)
 content = request.json()
 
-
-body =""
-for articles in content["articles"]:
-    body = body + articles["title"] + "\n" + articles["description"]+2*"\n"
-
-
+body = ""
+for article in content["articles"][0:20]:
+    title = article["title"] if article["title"] is not None else "No Title"
+    description = article["description"] if article["description"] is not None else "No Description"
+    url = article["url"] if article["url"] is not None else "No URL"
+    body = "Subject:Today's news" + "\n" + body + article["title"] + "\n" \
+           + article["description"] \
+           + "\n" + article["url"] + 2 * "\n"
 
 body = body.encode("utf-8")
 send_email(message=body)
