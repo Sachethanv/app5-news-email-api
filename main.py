7b2ea1d6d110009ea43684a88042a86b4ac9f98f
Sachethanv <Sachethanv.codes@gmail.com>
2025-01-01 17:55:17 +0530

    Update main.py to handle subject

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 import requests
 from mail import send_email
 
+# News API Configuration
 topic = "cricket"
 api_key = "fcb0f287b9af4b698812a5172e49bb9d"
-url = "https://newsapi.org/v2/everything?"\
-    f"q={topic}"\
-    "&from=2024-12-01"\
-    "&sortBy=publishedAt"\
-    "&apiKey=fcb0f287b9af4b698812a5172e49bb9d"\
+url = (
+    "https://newsapi.org/v2/everything?"
+    f"q={topic}"
+    "&from=2024-12-01"
+    "&sortBy=publishedAt"
+    "&apiKey=fcb0f287b9af4b698812a5172e49bb9d"
     "&language=en"
+)
 
+# Fetch news articles
 request = requests.get(url)
 content = request.json()
 
+# Prepare the email content
 body = ""
-for article in content["articles"][0:20]:
-    title = article["title"] if article["title"] is not None else "No Title"
-    description = article["description"] if article["description"] is not None else "No Description"
-    url = article["url"] if article["url"] is not None else "No URL"
-    body = "Subject:Today's news" + "\n" + body + article["title"] + "\n" \
-           + article["description"] \
-           + "\n" + article["url"] + 2 * "\n"
+for article in content["articles"][:20]:
+    title = article["title"] if article["title"] else "No Title"
+    description = article["description"] if article["description"] else "No Description"
+    url = article["url"] if article["url"] else "No URL"
+    body += f"{title}\n{description}\n{url}\n\n"
 
-body = body.encode("utf-8")
+# Send the email
 send_email(message=body)
